ec56684a3cc164ed97aac0ae6880e16140030060
sailendrachettri <sailendra9083@gmail.com>
2024-12-05 23:54:52 +0530

    stable version published

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-## Write better console logs
+## Logger helps you write better console logs
+
+### Also available for yarn
+`yarn add @bytesberry/logger`
+
+### Available Methods
+
+| Method      | Description                                                                 | Example Usage                               |
+|-------------|-----------------------------------------------------------------------------|--------------------------------------------|
+| `.output()` | Displays the given message in green to indicate success or standard output. | `Log.output('Some message', outputArr);` `Log.output('Some message', {outputArr});` |
+| `.error()`  | Logs the message to the console in red, typically used for errors or warnings. | `Log.error('An error occurred!');`        |
+| `.info()`   | Displays informational messages in teal, ideal for general or debug information. | `Log.info('Fetching data...');`          |
+
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,49 @@
-
 export class Log {
-    static output(msg: string){
-        console.log(`%c ${msg}`, 'color: green');
+    static output(msg: any, value?: any) {
+        if (typeof msg === 'object' && !value) {
+            // Case 1: msg is an object/array and no value
+            console.log(`%c`, 'color: green', msg);
+        } else if (typeof value === 'object') {
+            // Case 2: message is a string, value is an object/array
+            console.log(`%c${msg} ${JSON.stringify(value, null, 2)}`, 'color: green');
+        } else if (typeof msg === 'string' && !value) {
+            // Case 3: message is a string, no value
+            console.log(`%c ${msg}`, 'color: green');
+        } else if (typeof msg === 'string' && typeof value === 'string') {
+            // Case 4: both msg and value are strings
+            console.log(`%c ${msg} ${value}`, 'color: green');
+        }
     }
 
-    static error(msg:string){
-        console.log(`%c ${msg}`, 'color: red');
+    static error(msg: any, value?: any) {
+        if (typeof msg === 'object' && !value) {
+            // Case 1: msg is an object/array and no value
+            console.log(`%c`, 'color: red', msg);
+        } else if (typeof value === 'object') {
+            // Case 2: message is a string, value is an object/array
+            console.log(`%c${msg} ${JSON.stringify(value, null, 2)}`, 'color: red');
+        } else if (typeof msg === 'string' && !value) {
+            // Case 3: message is a string, no value
+            console.log(`%c ${msg}`, 'color: red');
+        } else if (typeof msg === 'string' && typeof value === 'string') {
+            // Case 4: both msg and value are strings
+            console.log(`%c ${msg} ${value}`, 'color: red');
+        }
     }
 
-    static info(msg: string){
-        console.log(`%c ${msg}`, 'color: teal');
+    static info(msg: any, value?: any) {
+        if (typeof msg === 'object' && !value) {
+            // Case 1: msg is an object/array and no value
+            console.log(`%c`, 'color: teal', msg);
+        } else if (typeof value === 'object') {
+            // Case 2: message is a string, value is an object/array
+            console.log(`%c${msg} ${JSON.stringify(value, null, 2)}`, 'color: teal');
+        } else if (typeof msg === 'string' && !value) {
+            // Case 3: message is a string, no value
+            console.log(`%c ${msg}`, 'color: teal');
+        } else if (typeof msg === 'string' && typeof value === 'string') {
+            // Case 4: both msg and value are strings
+            console.log(`%c ${msg} ${value}`, 'color: teal');
+        }
     }
 }
